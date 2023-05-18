5fb4c6dcce5ab82825622e07861e13e937229908
Marcos <marcosmelo012@gmail.com>
2023-05-18 19:14:37 -0300

    fix: indentation

--- build.sh
+++ build.sh
@@ -9,8 +9,8 @@ python3.9 -m pip install -r requirements.txt
 
 
 echo "Make Migrations"
-python3.9 manage.py makemigrations --noinput
-python3.9 manage.py migrate --noinput
+python3.11.0 manage.py makemigrations --noinput
+python3.11.0 manage.py migrate --noinput
 
 
 echo "Collect Static..."
