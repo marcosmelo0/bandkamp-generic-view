f5c33fb9d7a4e2c1e0beace79eaeb1eddfdfda31
Marcos <marcosmelo012@gmail.com>
2023-05-18 19:31:26 -0300

    fix: indentation

--- build.sh
+++ build.sh
@@ -5,16 +5,16 @@
 
 
 echo "Building the project..."
-python -m pip install -r requirements.txt
+python3.11.2 -m pip install -r requirements.txt
 
 
 echo "Make Migrations"
-python manage.py makemigrations --noinput
-python manage.py migrate --noinput
+python3.11.2 manage.py makemigrations --noinput
+python3.11.2 manage.py migrate --noinput
 
 
 echo "Collect Static..."
-python manage.py collectstatic --no-input --clear
+python3.11.2 manage.py collectstatic --no-input --clear
 
 
 echo "BUILD END"
